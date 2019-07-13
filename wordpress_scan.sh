5e5d65aed5e24135bc2a6f6cbf4d6c06ed68863d
defender <jacobo@defensahacker.com>
2019-07-13 13:27:05 +0200

    added more wordpress paths

--- wordpress_scan.sh
+++ wordpress_scan.sh
@@ -2,9 +2,10 @@
 
 domain=$1
 
-wpaths="/readme.html /wp-admin/upgrade.php /wp-admin/install.php /wp-login.php /wp-admin/setup-config.php /license.txt /wp-includes/wlwmanifest.xml /wp-config-sample.php"
+wpaths="/readme.html /wp-admin/upgrade.php /wp-admin/update-core.php /wp-admin/install.php /wp-admin/import.php /wp-login.php /wp-admin/setup-config.php /license.txt /wp-includes/wlwmanifest.xml /wp-config-sample.php"
 
 for i in $wpaths; do
-	echo $domain$i
+	echo
+	echo "[*] $domain$i"
 	curl -v $domain$i
 done
